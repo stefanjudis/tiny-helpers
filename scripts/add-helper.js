5851eb5e59f8e68d166c3513f5355aa462797586
stefan judis <stefanjudis@gmail.com>
2019-12-29 17:06:14 +0100

    add helper:add logic

--- scripts/add-helper.js
+++ scripts/add-helper.js
@@ -1,15 +1,52 @@
 const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+const { join } = require('path');
 
 (async () => {
-  const answers = await inquirer.prompt([
-    { name: 'name', type: 'input', message: 'Enter the name:' },
-    { name: 'desc', type: 'input', message: 'Enter a description:' },
-    { name: 'url', type: 'input', message: 'Enter a URL:' },
-    {
-      name: 'tags',
-      type: 'checkbox',
-      message: 'Pick some tags:',
-      choices: ['html', 'css', 'javascript', 'a11y', 'performance', 'design']
-    }
-  ]);
+  try {
+    let helpers = require('../data/helpers.json');
+    let tags = [
+      ...helpers.reduce((acc, cur) => {
+        cur.tags.forEach(tag => acc.add(tag));
+        return acc;
+      }, new Set())
+    ].sort((a, b) => (a < b ? -1 : 1));
+
+    const newHelper = await inquirer.prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Enter the name of the helper tool:'
+      },
+      { name: 'desc', type: 'input', message: 'Enter a description:' },
+      { name: 'url', type: 'input', message: 'Enter a URL:' },
+      {
+        name: 'tags',
+        type: 'checkbox',
+        message: 'Pick some tags:',
+        choices: tags
+      },
+      {
+        name: 'maintainers',
+        type: 'input',
+        message: 'Enter GitHub handles of maintainers (comma separated):'
+      }
+    ]);
+
+    newHelper.addedAt = new Date().toISOString().substring(0, 10);
+    newHelper.maintainers = newHelper.maintainers.split(',');
+    helpers.push(newHelper);
+    helpers = helpers.sort((a, b) =>
+      a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+    );
+
+    await writeFile(
+      join('data', 'helpers.json'),
+      JSON.stringify(helpers, null, 2)
+    );
+
+    console.log('Thanks!!! `./data/helpers.json` was updated');
+  } catch (error) {
+    console.error(error);
+  }
 })();
