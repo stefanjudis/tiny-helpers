76c0b177ceaa67d9640ee7ad43b6d53119a7e51a
stefan judis <stefanjudis@gmail.com>
2020-01-15 08:56:45 +0100

    Handle GitHub rate limiting

--- site/_data/contributors.js
+++ site/_data/contributors.js
@@ -1,11 +1,15 @@
 const got = require('got');
 
 module.exports = async function() {
-  const response = await got(
-    'https://api.github.com/repos/stefanjudis/tiny-helpers/contributors'
-  );
+  try {
+    const response = await got(
+      'https://api.github.com/repos/stefanjudis/tiny-helpers/contributors'
+    );
 
-  return JSON.parse(response.body)
-    .map(contributor => contributor.login)
-    .filter(contributor => contributor !== 'stefanjudis');
+    return JSON.parse(response.body)
+      .map(contributor => contributor.login)
+      .filter(contributor => contributor !== 'stefanjudis');
+  } catch (e) {
+    return [];
+  }
 };
