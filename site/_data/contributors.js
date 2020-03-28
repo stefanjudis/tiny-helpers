c8ac2069d8d0f90218da41eff90ca139e74e97cd
stefan judis <stefanjudis@gmail.com>
2020-03-28 11:59:32 +0100

    Debug NODE_ENV

--- site/_data/contributors.js
+++ site/_data/contributors.js
@@ -2,6 +2,7 @@ const got = require('got');
 
 module.exports = async function() {
   // don't hit the API on every rebuild due to rate limits
+  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'production') {
     try {
       const response = await got(
