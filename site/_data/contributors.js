61d189299d83a6278b47e62ec29ba49032dd225f
stefan judis <stefanjudis@gmail.com>
2020-05-26 10:10:35 +0200

    Show all contributors

--- site/_data/contributors.js
+++ site/_data/contributors.js
@@ -1,26 +1,42 @@
 const got = require('got');
 
-module.exports = async function() {
-  // don't hit the API on every rebuild due to rate limits
+async function fetchContributors({ page = 1, options }) {
+  const response = await got({
+    url: `https://api.github.com/repos/stefanjudis/tiny-helpers/contributors?per_page=100&page=${page}`,
+    ...options,
+  });
+
+  const contributors = JSON.parse(response.body)
+    .map((contributor) => contributor.login)
+    .filter((contributor) => contributor !== 'stefanjudis');
+
+  const match = response.headers.link.match(
+    /^<.*?&page=(?<nextPage>.*?)>; rel="next".*$/
+  );
+
+  return match
+    ? [
+        ...contributors,
+        ...(await fetchContributors({ page: +match.groups.nextPage, options })),
+      ]
+    : contributors;
+}
+
+module.exports = async function () {
+  //  don't hit the API on every rebuild due to rate limits
   if (process.env.NODE_ENV === 'production') {
     try {
-      const options = {};
       const { GITHUB_ACCESS_TOKEN } = process.env;
-
-      if (GITHUB_ACCESS_TOKEN) {
-        options.headers = {
-          authorization: `token ${GITHUB_ACCESS_TOKEN}`
-        };
+      if (!GITHUB_ACCESS_TOKEN) {
+        throw new Error('Please define GITHUB_ACCESS_TOKEN');
       }
+      const options = {
+        headers: {
+          authorization: `token 6e4e03c71b3372b70e391b281335eec349adbb17`,
+        },
+      };
 
-      const response = await got(
-        'https://api.github.com/repos/stefanjudis/tiny-helpers/contributors?per_page=100',
-        options
-      );
-
-      return JSON.parse(response.body)
-        .map(contributor => contributor.login)
-        .filter(contributor => contributor !== 'stefanjudis');
+      return await fetchContributors({ options });
     } catch (e) {
       console.error(e);
       return [];
