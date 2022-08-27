ae18a50d67e19dd1d91a10ffda9a5ad65380e612
stefan judis <stefanjudis@gmail.com>
2022-08-27 22:03:55 +0200

    Fix bug

--- api/screenshot.js
+++ api/screenshot.js
@@ -13,7 +13,7 @@ async function getBrowser() {
     });
 
     return puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
       headless: true,
       ignoreHTTPSErrors: true,
     });
