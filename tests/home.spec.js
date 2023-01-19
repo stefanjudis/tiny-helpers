5cb0d8ac812a6fffdb802b498f0f26b8b217b14d
stefan judis <stefanjudis@gmail.com>
2023-01-19 11:27:25 +0100

    Rename test

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -2,10 +2,10 @@ const { test, expect } = require('@playwright/test');
 
 const pageUrl = process.env.ENVIRONMENT_URL || 'https://www.tiny-helpers.dev';
 
-test('Mac Demo Home', async ({ page }) => {
+test('Home has the correct title', async ({ page }) => {
   console.log(`Using pageUrl: ${pageUrl}`);
   const response = await page.goto(pageUrl);
   expect(response.status()).toBe(200);
-  await expect(page).toHaveTitle(/Tiny Helperssss/);
+  await expect(page).toHaveTitle(/Tiny Helpers/);
   await page.screenshot({ path: 'home.jpg' });
 });
